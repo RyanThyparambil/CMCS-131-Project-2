4a907bbc6f1350d6ae70339a5eb26f3f81e45eaa
daniel <danield4443@gmail.com>
2026-02-26 12:08:53 -0500

    fixed some errors on WritePatient method in Simulation class.

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,14 +1,21 @@
+import java.io.*;
+
 public class Simulation {
-    public boolean WritePatients(String filename) {
+    public boolean WritePatient(String filename) {
         boolean result = true;
         File file = new File(filename);
         FileWriter writer = null;
         try {
             writer = new FileWriter(file);
-            for (int i = 0; i < patients().length; i++) {
-                if (patients()[i] != null) {
-                    writer.write(patients()[i].toCSV() + "\n");
+            for (int i = 0; i < Patient.length; i++) {
+                if (Patient[i] != null) {
+                    writer.write(Patient[i].toCSV() + "\n");
                 }
             }
+        } catch (IOException e) {
+            System.out.println("Patient Does Not Exist:" + filename);
+            result = false;
         }
+        return result;
     }
+}
