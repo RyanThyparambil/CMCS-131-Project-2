db148566419754d344e38c56071659bc9e748d21
daniel <danield4443@gmail.com>
2026-02-26 11:48:48 -0500

    Added WritePatients method to Simulation class

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,3 +1,14 @@
 public class Simulation {
-
-}
+    public boolean WritePatients(String filename) {
+        boolean result = true;
+        File file = new File(filename);
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(file);
+            for (int i = 0; i < patients().length; i++) {
+                if (patients()[i] != null) {
+                    writer.write(patients()[i].toCSV() + "\n");
+                }
+            }
+        }
+    }
