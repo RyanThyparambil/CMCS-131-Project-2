340d35b196cf515a2d7dea84ae959465bc5606a6
W1332864 <ryan.thybil1@gmail.com>
2026-02-26 12:13:33 -0500

    created name and dob for patient class and added back junit.

--- src/Patient.java
+++ src/Patient.java
@@ -3,9 +3,13 @@ import java.util.UUID;
 public class Patient {
 
     private UUID id;
+    private String Name;
+    private String Dob;
 
-    public Patient(){
+    public Patient(String name, String dob){
         this.id=UUID.randomUUID();
+        this.Name=name;
+        this.Dob=dob;
     }
 
     static Patient makePatient(){
