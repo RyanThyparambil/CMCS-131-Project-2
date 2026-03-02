90d5ed2ca4953851b6ca072a394c9ce735d38e68
RyanThyparambil <ryan.thybil1@gmail.com>
2026-03-02 11:59:13 -0500

    Added get ID and method for CSV file

--- src/Patient.java
+++ src/Patient.java
@@ -8,10 +8,21 @@ public class Patient {
         this.id=UUID.randomUUID();
     }
 
-
+    public static Patient createPatient() {
+        return new Patient();
+    }
 
     public UUID getID(){
         return id;
+    }
+
+    public String toCSV() {
+        return id.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Patient ID: " + id.toString();
     }
 
 
